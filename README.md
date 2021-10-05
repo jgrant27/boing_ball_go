201bb775e593d45811aa9a3d8d8c73334c2c8dea
Justin Grant <jgrant27@gmail.com>
2021-10-05 10:33:57 -0700

    Fill in remaining funcs and bouncing !

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Amiga Boing Ball in Go !
+# Boing Ball in Go !
 
+[Amiga Boing Ball 1984 demo](http://amiga.lychesis.net/special/ColorCycling/Boing.html)
+
+![BoingBall](img/boing.gif)
+
+## Instructions
+
+Run with `make run`
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"math"
 	"os"
 
+	"github.com/veandco/go-sdl2/gfx"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -18,22 +19,21 @@ const (
 var (
 	window *sdl.Window
 	render *sdl.Renderer
-	evt    *sdl.Event
+	right  = true
 	scale  = 120.0
 	phase  = 0.0
 	dp     = 2.5
-	x      = 320.0
 	dx     = 2.1
-	right  = true
 	y_ang  = 0.0
-	y      = 0.0
+	px     = 320.0
+	py     = 0.0
 )
 
 func main() {
 	sdl.Init(sdl.INIT_EVERYTHING)
 
 	window, _ = sdl.CreateWindow("Amiga Boing Ball", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 640, 512, sdl.WINDOW_OPENGL)
-	render, _ = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC|sdl.RENDERER_TARGETTEXTURE)
+	render, _ = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 
 	for {
 		listen_for_events()
@@ -49,18 +49,17 @@ func run_loop() {
 	clear_background()
 	do_physics()
 	var points = calc_points(math.Mod(phase, 22.5))
-	transform(points, scale, x, y)
+	transform(points)
 	draw_shadow(points)
-	draw_wireframe()
+	draw_grid()
 	fill_tiles(points, phase >= 22.5)
-
 }
 
 func listen_for_events() {
 	for {
 		evt := sdl.PollEvent()
-		keys := sdl.GetKeyboardState()
-		if (evt != nil && evt.GetType() == sdl.QUIT) || keys[sdl.K_ESCAPE] != 0 {
+		escape_pressed := sdl.GetKeyboardState()[sdl.GetScancodeFromKey(sdl.K_ESCAPE)] != 0
+		if escape_pressed || (evt != nil && evt.GetType() == sdl.QUIT) {
 			sdl.Quit()
 			os.Exit(0)
 		}
@@ -87,17 +86,17 @@ func do_physics() {
 	}
 	phase = math.Mod(phase+phase_shift, 45.0)
 	if right {
-		x += dx
+		px += dx
 	} else {
-		x -= dx
+		px -= dx
 	}
-	if x >= 505 {
+	if px >= 505 {
 		right = false
-	} else if x <= 135 {
+	} else if px <= 135 {
 		right = true
 	}
 	y_ang = math.Mod((y_ang + 1.5), 360.0)
-	y = 350.0 - 200.0*math.Abs(math.Cos(y_ang*math.Pi/180.0))
+	py = 350.0 - 200.0*math.Abs(math.Cos(y_ang*math.Pi/180.0))
 }
 
 func get_lat(phase float64, i int) float64 {
@@ -106,75 +105,88 @@ func get_lat(phase float64, i int) float64 {
 	} else if i == 9 {
 		return 90.0
 	} else {
-		return -90.0 + phase + (float64(i)-1.0)*22.5
+		return -90.0 + phase + ((float64(i) - 1.0) * 22.5)
 	}
 }
 
-func calc_points(phase float64) Sphere {
-	points := Sphere{}
-	sin_lat := make([]float64, 10)[:10]
+func calc_points(phase float64) *Sphere {
+	sin_lat := make([]float64, 10)
 	for i := 0; i < len(sin_lat); i++ {
 		lat := get_lat(phase, i)
 		sin_lat[i] = math.Sin(lat * math.Pi / 180.0)
 	}
+
+	points := Sphere{}
 	for j := 0; j < len(sin_lat)-1; j++ {
 		lon := -90.0 + float64(j)*22.5
 		y := math.Sin(lon * math.Pi / 180.0)
 		l := math.Cos(lon * math.Pi / 180.0)
-		for i := 0; i < len(sin_lat); i++ {
-			x := sin_lat[i] * l
-			points[i][j] = Point{x, y}
+		for k := 0; k < len(sin_lat); k++ {
+			x := sin_lat[k] * l
+			points[k][j] = Point{x, y}
 		}
 	}
 
-	return points
+	return &points
 }
 
-func transform(points Sphere, s float64, tx float64, ty float64) {
+func transform(points *Sphere) {
 	tilt_sphere(points, 17.0)
-	scale_and_translate(points, s, tx, ty)
+	scale_and_translate(points)
 }
 
-func tilt_sphere(points Sphere, ang float64) {
-
+func tilt_sphere(points *Sphere, ang float64) {
+	st := math.Sin(ang * math.Pi / 180.0)
+	ct := math.Cos(ang * math.Pi / 180.0)
+	for i := 0; i < len(points); i++ {
+		for j := 0; j < len(points); j++ {
+			pt := points[i][j]
+			x := pt[0]*ct - pt[1]*st
+			y := pt[0]*st + pt[1]*ct
+			points[i][j] = Point{x, y}
+		}
+	}
 }
 
-func scale_and_translate(points Sphere, s float64, tx float64, ty float64) {
-
+func scale_and_translate(points *Sphere) {
+	for i := 0; i < len(points); i++ {
+		for j := 0; j < len(points); j++ {
+			pt := points[i][j]
+			x := pt[0]*scale + px
+			y := pt[1]*scale + py
+			points[i][j] = Point{x, y}
+		}
+	}
 }
 
-func draw_shadow(points Sphere) {
-	// var polyX, polyY: array[0..16, int16]
+func draw_shadow(points *Sphere) {
+	poly_x, poly_y := make([]int16, 16), make([]int16, 16)
 
-	// for i in 0..8:
-	//	let p = points[0][i]
-	//	polyX[i] = int16(p[0]) + 50
-	//	polyY[i] = int16(p[1])
-	// for i in 0..8:
-	//	let p = points[9][8 - i]
-	//	polyX[7 + i] = int16(p[0]) + 50
-	//	polyY[7 + i] = int16(p[1])
+	for i := 0; i <= 8; i++ {
+		p := points[0][i]
+		poly_x[i] = int16(p[0]) + 50
+		poly_y[i] = int16(p[1])
+	}
+	for i := 0; i <= 8; i++ {
+		p := points[9][8-i]
+		poly_x[7+i] = int16(p[0]) + 50
+		poly_y[7+i] = int16(p[1])
+	}
 
-	//	var //gray
-	//	r: uint8 = 102
-	//	g: uint8 = 102
-	//	b: uint8 = 102
-	// gfx.filledPolygonRGBA(render,
-	//	  vx = addr(polyX[0]), vy = addr(polyY[0]), n = 15, r, g, b, 255)
-
+	gfx.FilledPolygonRGBA(render, poly_x, poly_y, 102, 102, 102, 255) // dark gray
 }
 
-func draw_wireframe() {
+func draw_grid() {
 	render.SetDrawColor(183, 45, 168, 255) // purple
 	render.SetScale(1, 1)
 
-	var is = make([]int, 13)[:13]
+	var is = make([]int, 13)
 	for i := range is {
 		y := int32(i * 36)
 		render.DrawLine(50, y, 590, y)
 	}
 
-	is = make([]int, 16)[:16]
+	is = make([]int, 16)
 	for i := range is {
 		x := int32(50 + i*36)
 		render.DrawLine(x, 0, x, 432)
@@ -185,7 +197,7 @@ func draw_wireframe() {
 	}
 
 	ys := []int{442, 454, 468}
-	is = make([]int, 3)[:3]
+	is = make([]int, 3)
 	for i := range is {
 		y := ys[i]
 		x1 := 50.0 - 50.0*(float32(y)-432.0)/(480.0-432.0)
@@ -194,6 +206,32 @@ func draw_wireframe() {
 
 }
 
-func fill_tiles(points Sphere, alter bool) {
+func fill_tiles(points *Sphere, alter bool) {
 
+	poly_n := uint8(4)
+	for j := 0; j < 8; j++ {
+		for i := 0; i <= 8; i++ {
+			p1 := points[i][j]
+			p2 := points[i+1][j]
+			p3 := points[i+1][j+1]
+			p4 := points[i][j+1]
+			poly_x, poly_y := make([]int16, poly_n), make([]int16, poly_n)
+			poly_x[0] = int16(p1[0])
+			poly_y[0] = int16(p1[1])
+			poly_x[1] = int16(p2[0])
+			poly_y[1] = int16(p2[1])
+			poly_x[2] = int16(p3[0])
+			poly_y[2] = int16(p3[1])
+			poly_x[3] = int16(p4[0])
+			poly_y[3] = int16(p4[1])
+
+			r, g, b := uint8(255), uint8(255), uint8(255)
+			if alter {
+				r, g, b = uint8(255), uint8(0), uint8(0)
+			}
+			gfx.FilledPolygonRGBA(render, poly_x, poly_y, r, g, b, 255)
+
+			alter = !alter
+		}
+	}
 }
